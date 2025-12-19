f5ea2a64f44e1212bb9a8138ae9b8b42a2b5173d
Marvin Kohl <marvin.kohl@posteo.de>
2025-12-19 14:22:27 +0100

    write invalid_id for day2

--- day02.pl
+++ day02.pl
@@ -16,3 +16,21 @@ valid_program:-
     invalid_ids(range(38593856,38593862), [38593859]),
     added_up_ids([11,22,99,1010,1188511885,222222,446446,38593859], 1227775554),
     added_up_ids_in_file("day02_test_input.txt", 1227775554).
+
+invalid_id(Number):-
+    integer(Number),
+    number_string(Number, String),
+    invalid_id(String).
+invalid_id(Atom):-
+    atom(Atom),
+    atom_string(Atom, String),
+    invalid_id(String).
+invalid_id(Number):-
+    string(Number),
+    string_length(Number, Len),
+    Len mod 2 =:= 0,
+    Half is Len / 2,
+    sub_string(Number, 0, Half, Half, X),
+    sub_string(Number, Half, Half, 0, X).
+
+valid_id(X):- \+ invalid_id(X).
