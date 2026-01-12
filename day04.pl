66d7ecd5dc44f3e863e1577ff9fb0783a1f65932
Marvin Kohl <marvin.kohl@posteo.de>
2026-01-12 20:08:27 +0100

    Implement file reading for day 04

--- day04.pl
+++ day04.pl
@@ -11,3 +11,17 @@ main(X):-
     accessible_rolls_of_paper("day04_input.txt", X).
 
 accessible_rolls_of_paper(_, _):- fail.
+
+%% Reading the Diagram from a File as list of list of chars.
+file_diagram(File, Diagram):-
+    open(File, read, Stream),
+    stream_diagram(Stream, Diagram),
+    !,
+    close(Stream).
+
+stream_diagram(Stream, []):- at_end_of_stream(Stream).
+stream_diagram(Stream, [Line|RestLines]):-
+    \+ at_end_of_stream(Stream),
+    read_line_to_codes(Stream, Codes), !,
+    maplist(char_code, Line, Codes),
+    stream_diagram(Stream, RestLines).
