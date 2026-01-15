7a8c42a2cad4549a857d96cf1beb17bde1595d63
Marvin Kohl <marvin.kohl@posteo.de>
2026-01-15 23:06:03 +0100

    First try to implement adjacent_items/3.

--- day04.pl
+++ day04.pl
@@ -25,3 +25,25 @@ stream_diagram(Stream, [Line|RestLines]):-
     read_line_to_codes(Stream, Codes), !,
     maplist(char_code, Line, Codes),
     stream_diagram(Stream, RestLines).
+
+%%
+adjacent_items(Diagram, index(X, Y), Items):-
+    findall(X, adjacent_item(Diagram, index(X, Y), X), Items),
+    assertion((length(Items, Len), Len =< 8)).
+
+adjacent_item(Diagram, Index, Item):-
+    adjacent_index(Index, AdjIndex),
+    indexed_value(Diagram, AdjIndex, Item).
+
+adjacent_index(index(X, Y), index(X2, Y2)):- X2 is X - 1, Y2 is Y - 1.
+adjacent_index(index(X, Y), index(X2, Y)):- X2 is X - 1.
+adjacent_index(index(X, Y), index(X2, Y2)):- X2 is X - 1, Y2 is Y + 1.
+adjacent_index(index(X, Y), index(X, Y2)):- Y2 is Y - 1.
+adjacent_index(index(X, Y), index(X, Y2)):- Y2 is Y + 1.
+adjacent_index(index(X, Y), index(X2, Y2)):- X2 is X + 1, Y2 is Y - 1.
+adjacent_index(index(X, Y), index(X2, Y)):- X2 is X + 1.
+adjacent_index(index(X, Y), index(X2, Y2)):- X2 is X + 1, Y2 is Y + 1.
+
+indexed_value(Diagram, index(X, Y), Item):-
+    nth0(X, Diagram, Row),
+    nth0(Y, Row, Item).
