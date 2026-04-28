4aaa5389f3b8f24640294a3c4e4950a9122595d3
Marvin Kohl <marvin.kohl@posteo.de>
2026-04-28 21:11:17 +0200

    Implement loading fresh ingredients from file

--- day05.pl
+++ day05.pl
@@ -8,7 +8,8 @@ file_ingredients(File, Fresh, Available):-
 
 stream_ingredients(Stream, Fresh, Available):-
     stream_lines(Stream, Lines),
-    append([FreshLines, [], AvailableLines], Lines),
+    append([FreshLines, [""], AvailableLines], Lines),
+    maplist(string_range, FreshLines, Fresh),
     maplist(number_string, Available, AvailableLines).
 
 stream_lines(Stream, []):- at_end_of_stream(Stream).
@@ -17,3 +18,12 @@ stream_lines(Stream, [Line|RestLines]):-
     read_line_to_codes(Stream, Codes), !,
     string_codes(Line, Codes),
     stream_lines(Stream, RestLines).
+
+%%! string_range(+String:string, Range)
+string_range(String, range(X, Y)):-
+    debug(day05, "string_range ~s~n", [String]),
+    sub_string(String, XLen, 1, YLen, "-"),
+    sub_string(String, 0, XLen, _, XStr),
+    sub_string(String, _, YLen, 0, YStr),
+    number_string(X, XStr),
+    number_string(Y, YStr).
--- test_day05.pl
+++ test_day05.pl
@@ -8,4 +8,8 @@ test(available_ingedrients_are_loaded):-
     file_ingredients("day05_test_input.txt", _, Result),
     assertion(Result == [1, 5, 8, 11, 17, 32]).
 
+test(fresh_ingredients_are_loaded):-
+    file_ingredients("day05_test_input.txt", Result, _),
+    assertion(Result == [range(3,5), range(10,14), range(16,20), range(12,18)]).
+
 :- end_tests(day05).
