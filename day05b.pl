93c87b663ba59674b76b9730caab586bab6e6f76
Marvin Kohl <marvin.kohl@posteo.de>
2026-06-14 11:30:11 +0200

    Optimize und complete day5b
    The result of day05b is 344306344403172.

--- day05b.pl
+++ day05b.pl
@@ -9,7 +9,28 @@
 %  True if Count is the count of all fresh ingredients in the File even
 %  when unavailable.
 file_fresh_ingredient_count(File, Count):-
-    file_ingredients(File, Fresh1, _),
-    range_list_merged(Fresh1, Fresh),
-    findall(X, range_member(Fresh, X), Members),
-    length(Members, Count).
+    file_ingredients(File, Fresh, _),
+    range_member_count(Fresh, Count).
+
+%% range_member_count(+Range:range, -Count:int)
+%
+%  True if Count is the count of all members in Range.
+range_member_count(Range, Count):-
+    is_single_range(Range),
+    var(Count),
+    !,
+    range(X, Y) = Range,
+    Count is Y - X + 1.
+range_member_count(Range, Count):-
+    is_range_list(Range),
+    var(Count),
+    !,
+    range_list_merged(Range, Merged),
+    range_member_count(Merged, 0, Count).
+
+%% range_member_count(+Range:range, +Acc:int, -Count:int)
+range_member_count([], Count, Count).
+range_member_count([R1|Rs], A0, Count):-
+    range_member_count(R1, C1),
+    A1 is A0 + C1,
+    range_member_count(Rs, A1, Count).
