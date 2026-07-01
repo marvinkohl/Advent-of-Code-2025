14339b8917db3413bb018ad00b812f7744219bac
Marvin Kohl <marvin.kohl@posteo.de>
2026-07-01 23:03:34 +0200

    day06: Implement problem_result/2

--- day06.pl
+++ day06.pl
@@ -12,3 +12,11 @@
 %% problem_result(+Problem, -Result:integer) is det
 %
 %  True if Result is the calculated number of the Problem.
+
+problem_result(problem(Numbers, '*'), Result):-
+    foldl(product, Numbers, 1, Result).
+problem_result(problem(Numbers, '+'), Result):-
+    foldl(plus, Numbers, 0, Result).
+
+product(N1, N2, Product):-
+    Product is N1 * N2.
--- test_day06.pl
+++ test_day06.pl
@@ -5,4 +5,9 @@
 
 :- begin_tests(day06).
 
+test(problem_result, Result == 33210):-
+    problem_result(problem([123, 45, 6], '*'), Result).
+test(problem_result, Result == 490):-
+    problem_result(problem([328, 64, 98], '+'), Result).
+
 :- end_tests(day06).
